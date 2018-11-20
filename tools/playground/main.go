c82f914aa3f1573da4b48cc9a9c062f43a440b83
Hank Gao <hankgao@gmail.com>
2018-11-20 09:06:02 +0800

    lazy comment

--- tools/playground/main.go
+++ tools/playground/main.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/hankgao/vote-server/src/api"
 )
 
 func main() {
-	ticker2S := time.NewTicker(time.Second * 1)
 
-	go func() {
-		for {
-			select {
-			case <-ticker2S.C:
-				fmt.Println("2 seconds passed")
-			}
-		}
+	defer api.CloseDatabaseConn()
 
-	}()
+	coins := api.RetrieveProjectCoins("Open")
+	for _, c := range coins {
+		fmt.Println(c.Name)
+	}
 
-	t := time.NewTimer(time.Minute * 1)
-	<-t.C
+	// ticker2S := time.NewTicker(time.Second * 1)
 
-	ticker2S.Stop()
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-ticker2S.C:
+	// 			fmt.Println("2 seconds passed")
+	// 		}
+	// 	}
 
-	fmt.Println("Done")
+	// }()
+
+	// t := time.NewTimer(time.Minute * 1)
+	// <-t.C
+
+	// ticker2S.Stop()
+
+	// fmt.Println("Done")
 
 }
