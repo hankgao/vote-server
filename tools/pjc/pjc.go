127eedfc49c028cbe72347e355cd72a62ab0d32d
Hank Gao <hankgao@gmail.com>
2018-11-21 19:37:48 +0800

    load project coin

--- tools/pjc/pjc.go
+++ tools/pjc/pjc.go
@@ -4,19 +4,32 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
 
 	"github.com/hankgao/vote-server/src/api"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: pjc <xxx.json>\n")
+		return
+	}
 	// load from a JSON file
 	coin := api.ProjectCoin{}
-	err := coin.Load("xxcoin.json")
+	err := coin.Load(os.Args[1])
+
 	if err != nil {
+		fmt.Printf("failed to load project coin: %s\n", err)
 		return
 	}
 
 	// Insert the newly loaded coin into database
+	err = api.AddProjectCoin(coin)
+	if err != nil {
+		fmt.Printf("failed to add project coin: %s\n", err)
+		return
+	}
 
 }
 
